d83688177ccefe6b1dc2656479d0daad41d359b1
ginowine <ginowine1@gmail.com>
2020-05-21 04:10:55 +0100

    text to get car by ID implemented

--- src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -40,6 +41,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
+@WebMvcTest(CarController.class)
 public class CarControllerTest {
 
     @Autowired
